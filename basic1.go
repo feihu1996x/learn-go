0ada4f93760632108f2038c0fde7d57ed21b4730
feihu1996x <gsx0312@qq.com>
2018-07-23 15:31:40 +0800

    update file

--- basic1.go
+++ basic1.go
@@ -34,34 +34,34 @@
 package main
 
 import (
-	"fmt"
-	"math/cmplx"
-	"math"
+    "fmt"
+    "math/cmplx"
+    "math"
 )
 
 func euler() {
- 	//c := 3 + 4i
- 	//fmt.Println(cmplx.Abs(c)) // 复数取模
+     //c := 3 + 4i
+     //fmt.Println(cmplx.Abs(c)) // 复数取模
 
- 	/*
- 	欧拉公式
- 	*/
- 	fmt.Println(cmplx.Pow(math.E, 1i * math.Pi) + 1)
-	fmt.Println(cmplx.Exp(1i * math.Pi) + 1)
-	fmt.Printf("%.3f\n", cmplx.Exp(1i * math.Pi) + 1)
+     /*
+     欧拉公式
+     */
+     fmt.Println(cmplx.Pow(math.E, 1i * math.Pi) + 1)
+    fmt.Println(cmplx.Exp(1i * math.Pi) + 1)
+    fmt.Printf("%.3f\n", cmplx.Exp(1i * math.Pi) + 1)
 }
 
 func triangle() {
-	/*
-	强制类型转换
-	*/
-	a, b := 3, 4
-	var c int
-	c = int(math.Sqrt(float64(a*a + b*b)))
-	fmt.Println(c)
+    /*
+    强制类型转换
+    */
+    a, b := 3, 4
+    var c int
+    c = int(math.Sqrt(float64(a*a + b*b)))
+    fmt.Println(c)
 }
 
 func main() {
-	euler()
-	triangle()
+    euler()
+    triangle()
 }
