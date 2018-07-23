2bccc984028d144bd86731bd9dee230fca11ebff
feihu1996x <gsx0312@qq.com>
2018-07-23 15:29:42 +0800

    learned 强制类型转换

--- basic1.go
+++ basic1.go
@@ -51,6 +51,17 @@ func euler() {
 	fmt.Printf("%.3f\n", cmplx.Exp(1i * math.Pi) + 1)
 }
 
+func triangle() {
+	/*
+	强制类型转换
+	*/
+	a, b := 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
+}
+
 func main() {
 	euler()
+	triangle()
 }
